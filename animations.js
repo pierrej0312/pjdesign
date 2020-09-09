35b58f8e925a2d8a7a0e79f707b0166b31f5c94d
Pierre <pierrej0312@gmail.com>
2020-09-09 21:28:01 +0200

    somes effects + inside nav

--- animations.js
+++ animations.js
@@ -15,7 +15,7 @@ window.addEventListener("scroll", ()=> {
     }
 });
 
-let scrollfadeonePanel = window.innerHeight - 500;
+let scrollfadeonePanel = window.innerHeight*0.5;
 
 window.addEventListener("scroll", ()=> {
     
@@ -23,7 +23,7 @@ window.addEventListener("scroll", ()=> {
         document.querySelector(".scrollfadeone.fade-down").classList.add("start");
     }
 });
-let scrollfadetwoPanel = window.innerHeight + 300;
+let scrollfadetwoPanel = window.innerHeight*1.4;
 
 window.addEventListener("scroll", ()=> {
     
@@ -34,7 +34,7 @@ window.addEventListener("scroll", ()=> {
     }
 });
 
-let scrollfadetreePanel = window.innerHeight*2 - 200;
+let scrollfadetreePanel = window.innerHeight*1.4 + 300;
 
 window.addEventListener("scroll", ()=> {
     
@@ -53,9 +53,59 @@ window.addEventListener("load", function() {
     document.querySelector('.main-anim.loading').classList.remove("loading");
     document.querySelector('.hero-anim.fade-down').classList.add("start");
     document.querySelector('.why-me.fade-left').classList.add("start");
+    document.querySelector('.inside-nav.fade-left').classList.add("start");
 });
 
+
+//inside-link active
+
+var z = document.getElementsByClassName("inside-link");
+
+var currentTab = 0;
+
+let topTop = document.querySelector('#top').offsetTop - 300;
+let latestWorksTop = document.querySelector('#latestworks').offsetTop - 300;
+let whymeTop = document.querySelector('#whyme').offsetTop - 200;
+let toplink = document.querySelector('.inside-link[href="#top"]');
+let latestWorkslink = document.querySelector('.inside-link[href="#latestworks"]');
+let whymelinks = document.querySelector('.inside-link[href="#whyme"]');
+
+window.addEventListener("scroll", ()=> {
+    
+    if(document.querySelector('#top') && (document.body.scrollTop > topTop || document.documentElement.scrollTop > topTop) && (document.body.scrollTop < latestWorksTop || document.documentElement.scrollTop < latestWorksTop)){
+        if(document.querySelector('.inside-link.active')) document.querySelector('.inside-link.active').classList.remove('active')
+        z[0].classList.add("active");
+    }
+    if(document.querySelector('#latestworks') && (document.body.scrollTop > latestWorksTop || document.documentElement.scrollTop > latestWorksTop) && (document.body.scrollTop < whymeTop || document.documentElement.scrollTop < whymeTop)){
+        if(document.querySelector('.inside-link.active')) document.querySelector('.inside-link.active').classList.remove('active')
+        z[1].classList.add("active");
+    }
+    if(document.querySelector('#whyme') && (document.body.scrollTop > whymeTop || document.documentElement.scrollTop > whymeTop)){
+        if(document.querySelector('.inside-link.active')) document.querySelector('.inside-link.active').classList.remove('active')
+        z[2].classList.add("active");
+
+    }
+    else {
+        return
+    }
+});
+
+
+
+function toscrollTop() {
+    document.documentElement.scrollTop = topTop
+};
+function toscrolllatestworks() {
+    document.documentElement.scrollTop = latestWorksTop
+};
+function toscrollwhyme() {
+    document.documentElement.scrollTop = whymeTop
+};
+
+
 //swipe car
+
+
 
 const _C = document.querySelector('.swipe-content'),
       N = document.querySelector('.swipe-content').children.length;
@@ -78,16 +128,25 @@ const _C = document.querySelector('.swipe-content'),
         
             x[n].classList.add('currentSlide');
             x[currentTab + 1].classList.add('nextSlide');
+            x[currentTab + 1].querySelector('figcaption').style.background = 'white';
             x[currentTab].classList.remove('nextSlide');
             x[currentTab].classList.remove('prevSlide');
             document.querySelector('.swipe-container').style.setProperty('--currentTab', currentTab);
             document.querySelector('.swipe-container').style.setProperty('--w', W + "px");
+
+            var figcap = x[currentTab].querySelector('figcaption');
+            figcap.style.background = "radial-gradient(256.55% 519.75% at 23.97% -78.29%, #E4665C 0.46%, #424F60 100%)";//figcap.getAttribute('data-color');
             //document.querySelector('.swipe-container').style.setProperty('--tx', '0px');
             //document.querySelector('.swipe-container').setAttribute('style','transform:translateX(calc(' + currentTab + '*(-' + w + 'px)));');
 
         if (n > 0 && n < (x.length - 2)) {
             x[currentTab - 1].classList.add('prevSlide');
+            x[currentTab - 1].querySelector('figcaption').style.background = 'white';
         }
+        /*if (n = 0 && n < (x.length - 3)) {
+            //x[currentTab + 1].classList.add('nextSlide');
+            //x[currentTab + 1].querySelector('figcaption').style.background = 'white';
+        }*/
 
         if (n == 0) {
             document.querySelector(".button-left.slider-button").style.display = "none";
@@ -99,6 +158,7 @@ const _C = document.querySelector('.swipe-content'),
           } else {
             document.querySelector(".button-right.slider-button").style.display = "flex";
         }
+        
         
         
         //document.querySelector('.swipe-container').style./*display = "none"; */transform = "translate-x(calc(" + (currentTab + 1) + "*-" + w + ")";
@@ -113,7 +173,9 @@ const _C = document.querySelector('.swipe-content'),
 
         x[currentTab].classList.remove("currentSlide");
 
+
         currentTab = currentTab + n;
+        
        
         showTab(currentTab);
     }
@@ -226,295 +288,3 @@ document.querySelector('.swipe-container').addEventListener('touchmove', e => {e
 document.querySelector('.swipe-container').addEventListener('mousemove', drag, false);
 document.querySelector('.swipe-container').addEventListener('touchmove', drag, false);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const _C = document.querySelector('.swipe-container');
-N = _C.children.length;
-_C.style.setProperty('--n', N);
-
-let locked = false;
-
-const TFN = {
-    'linear': function(k) {
-        return k
-    },
-    'ease-in': function(k, e = 1.675) {
-        return Math.pow(k, e)
-    },
-    'ease-out': function(k, e = 1.675) {
-        return 1 - Math.pow(1 - k, e)
-    },
-    'ease-in-out': function(k) {
-        return .5*(Math.sin((k - .5)*Math.PI) + 1)
-    },
-    'bounce-out': function(k, a = 2.75, b = 1.5) {
-        return 1 - Math.pow(1 - k, a)*Math.abs(Math.cos(Math.pow(k, b)*(n + .5)*Math.PI))
-    }
-};
-
-function unify(e) {
-    return e.changedTouches ? e.changedTouches[0] : e
-};
-
-let x0 = null;
-
-function lock(e) {
-    x0 = unify(e).clientX;
-    locked = true
-};
-
-function drag(e) {
-
-    e.preventDefault();
-
-    if(locked) {
-        let dx = unify(e).clientX - x0, f = +(dx/w).toFixed(2);
-        _C.style.setProperty('--i', i - f)
-    }   
-};
-
-let w;
-
-function size() {
-    w = window.innerWidth
-};
-
-let ini, fin;
-
-const NF = 30;
-
-let rID = null;
-
-function stopAni() {
-    cancelAnimationFrame(rID);
-    rID = null
-};
-
-function ani(cf = 0) {
-    _C.style.setProperty('--i', ini + (fin - ini)*TFN['ease-out'](cf/anf));
-
-    if(cf === anf) {
-        stopAni();
-        return
-    }
-
-    rID = requestAnimationFrame(ani.bind(this, ++cf))
-};
-
-var n;
-
-let i = 0;
-
-function move(e) {
-    if(locked) {
-        let dx = unify(e).clientX - x0, s = Math.sign(dx), f = +(s*dx/w).toFixed(2);
-        ini = i - s*f;
-
-        anf = Math.round(f*NF)
-        n = 2 + Math.round(f)
-
-        ani();
-
-        if ((i > 0 || s < 0) && (i < N - 1 || s > 0) && f > .2) {
-            i -= s;
-            f = 1 - f
-        }
-
-        fin = i;
-        ani();
-        x0 = null;
-        locked = false
-    }
-};
-
-addEventListener('resize', size, false);
-
-_C.addEventListener('mousedown', lock, false);
-_C.addEventListener('touchstart', lock, false);
-
-_C.addEventListener('mouseup', move, false);
-_C.addEventListener('touchend', move, false);
-
-_C.addEventListener('touchmove', e => {
-    e.preventDefault()
-}, false )
-
-_C.addEventListener('mousemove', drag, false);
-_C.addEventListener('touchmove', drag, false);*/
-
-//autre methode swipe a combiner avec https://inspiredwebdev.com/javascript-carousel
-
-/*
-let touchstartX = 0;
-let touchstartY = 0;
-let touchendX = 0;
-let touchendY = 0;
-
-const gestureZone = document.getElementById('modalContent');
-
-gestureZone.addEventListener('touchstart', function(event) {
-    touchstartX = event.changedTouches[0].screenX;
-    touchstartY = event.changedTouches[0].screenY;
-}, false);
-
-gestureZone.addEventListener('touchend', function(event) {
-    touchendX = event.changedTouches[0].screenX;
-    touchendY = event.changedTouches[0].screenY;
-    handleGesture();
-}, false); 
-
-function handleGesture() {
-    if (touchendX < touchstartX) {
-        console.log('Swiped left');
-    }
-    
-    if (touchendX > touchstartX) {
-        console.log('Swiped right');
-    }
-    
-    if (touchendY < touchstartY) {
-        console.log('Swiped up');
-    }
-    
-    if (touchendY > touchstartY) {
-       console.log('Swiped down');
-    }
-    
-    if (touchendY === touchstartY) {
-       console.log('Tap');
-    }
-}
-*/
-
-//ou
-
-
-
-
-
-
-
-
-/*
-let startX = 0;
-let startY = 0;
-
-const gestureZone = document.querySelector('.swipe-container');
-
-gestureZone.addEventListener('touchstart', function(event) {
-    handleTouchStart();
-}, false);
-
-gestureZone.addEventListener('touchend', function(event) {
-    handleTouchEnd();
-}, false); 
-
-function handleTouchStart(e) {
-  startX = e.changedTouches[0].screenX;
-  startY = e.changedTouches[0].screenY;
-}
-
-function handleTouchEnd(e) {
-  const diffX = e.changedTouches[0].screenX - startX;
-  const diffY = e.changedTouches[0].screenY - startY;
-  const ratioX = Math.abs(diffX / diffY);
-  const ratioY = Math.abs(diffY / diffX);
-  const absDiff = Math.abs(ratioX > ratioY ? diffX : diffY);
-
-  // Ignore small movements.
-  if (absDiff < 30) {
-    return;
-  }
-
-  if (ratioX > ratioY) {
-    if (diffX >= 0) {
-      console.log('right swipe');
-    } else {
-      console.log('left swipe');
-    }
-  } else {
-    if (diffY >= 0) {
-      console.log('down swipe');
-    } else {
-      console.log('up swipe');
-    }
-  }
-}
-
-
-
-
-
-
-
-
-let startX = 0;
-let startY = 0;
-
-const gestureZone = document.querySelector('.swipe-container');
-
-gestureZone.addEventListener('touchstart', function(event) {
-    handleTouchStart();
-}, false);
-
-gestureZone.addEventListener('touchend', function(event) {
-    handleTouchEnd();
-}, false); 
-
-function handleTouchStart(e) {
-  startX = e.changedTouches[0].screenX;
-  startY = e.changedTouches[0].screenY;
-}
-
-function handleTouchEnd(e) {
-  const diffX = e.changedTouches[0].screenX - startX;
-  const diffY = e.changedTouches[0].screenY - startY;
-  const ratioX = Math.abs(diffX / diffY);
-  const ratioY = Math.abs(diffY / diffX);
-  const absDiff = Math.abs(ratioX > ratioY ? diffX : diffY);
-
-  // Ignore small movements.
-  if (absDiff < 30) {
-    return;
-  }
-
-  if (ratioX > ratioY) {
-    if (diffX >= 0) {
-        var x = document.getElementsByClassName("swipe-content");
-
-        x[currentTab].classList.remove("currentSlide");
-
-        currentTab = currentTab + 1;
-       
-        showTab(currentTab);
-    } else {
-        var x = document.getElementsByClassName("swipe-content");
-
-        x[currentTab].classList.remove("currentSlide");
-
-        currentTab = currentTab - 1;
-       
-        showTab(currentTab);
-    }
-  } else {
-    if (diffY >= 0) {
-      console.log('down swipe');
-    } else {
-      console.log('up swipe');
-    }
-  }
-}
-*/
